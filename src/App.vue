4c6aa8cca78980e0c34a35099d75cbda19a60d98
Hasegawa Taiji <shortrange01@gmail.com>
2021-02-11 01:39:49 +0900

    delete: 不要ファイル削除

--- src/App.vue
+++ src/App.vue
@@ -5,64 +5,12 @@
 </template>
 
 <script lang="ts">
-import {defineComponent, reactive} from 'vue';
+import { defineComponent } from 'vue';
 import Header from './components/Header.vue';
-
-interface State {
-  message: string;
-}
 
 export default defineComponent({
   components: {
     Header
-  },
-  setup() {
-    const state = reactive({
-      message: 'Learn Vue',
-    });
-    return {
-      state,
-    };
-  },
+  }
 });
 </script>
-
-<style>
-.App {
-  text-align: center;
-}
-.App-header {
-  background-color: #f9f6f6;
-  color: #32485f;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-}
-.App-link {
-  color: #00c185;
-}
-.App-logo {
-  height: 40vmin;
-  pointer-events: none;
-  margin-bottom: 1rem;
-  animation: App-logo-spin infinite 1.6s ease-in-out alternate;
-}
-.App-tsx {
-  display: flex;
-}
-.App-tsx > div {
-  margin-left: 30px;
-  font-size: 16px;
-}
-@keyframes App-logo-spin {
-  from {
-    transform: scale(1);
-  }
-  to {
-    transform: scale(1.06);
-  }
-}
-</style>
