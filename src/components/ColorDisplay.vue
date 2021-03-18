5083557cf19d3df280cfac80381d5d3173b68b7c
Hasegawa Taiji <shortrange01@gmail.com>
2021-03-19 01:24:20 +0900

    feat: ColorDisplay component追加

---
+++ src/components/ColorDisplay.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="colorDisplay">
+        <div class="colorView" :style="{ 'background-color': colorCode }"></div>
+        <div class="text" :style="{ color: colorCode }">
+            <p>{{ colorCode }}</p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { PropType, computed, defineComponent, reactive } from "vue";
+
+export default defineComponent({
+    props: {
+        colorCode: {
+            type: String,
+        },
+    },
+});
+</script>
+
+<style lang="scss" scoped>
+.colorDisplay {
+    position: relative;
+    width: 100px;
+    text-align: center;
+    .colorView {
+        width: 100px;
+        height: 100px;
+        border-radius: 50%;
+    }
+    .text {
+        width: 100%;
+        margin-top: 4px;
+    }
+}
+</style>
